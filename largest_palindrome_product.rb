8227bd2d26a793821d94339552d11710ca163eae
John Kiragu <john.kiragu@jumo.world>
2017-08-17 09:45:38 +0300

    Initial commit working on largest palindrome product

---
+++ largest_palindrome_product.rb
@@ -0,0 +1,8 @@
+def largest_palindrome_product(n)
+    # @TODO finish up logic for this implementation
+end
+t = gets.strip.to_i
+for i in 0..t-1
+    n = gets.strip.to_i
+    puts largest_palindrome_product(n)
+end
